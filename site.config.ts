e71fcfaac20322bcbe6cda5a16e0acb62dcfd839
Shark_98 <67531616+YuuSuKei1998@users.noreply.github.com>
2022-09-28 17:53:31 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'e66e6fede30c4c81858b90c845da2495',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Sharks Home',
+  domain: 'https://yuusukei.notion.site/Shark-s-Home-e66e6fede30c4c81858b90c845da2495',
+  author: 'Yuusukei',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
